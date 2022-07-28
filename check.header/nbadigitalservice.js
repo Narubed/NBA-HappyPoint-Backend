3c01414995a543efd89deee0ef64b03013686511
Narubed Kaewserd <40911777+Narubed@users.noreply.github.com>
2022-07-28 15:39:41 +0700

    add

--- check.header/nbadigitalservice.js
+++ check.header/nbadigitalservice.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
+
 module.exports = usePoint = async (req, res) => {
-  console.log(req.headers);
-  if (!req.headers.secret_key || !req.headers.token_key) {
+  console.log(req.headers.origin);
+  if (req.headers.origin || req.headers.origin === "http://localhost:3000") {
+    return true;
+  } else if (!req.headers.secret_key || !req.headers.token_key) {
     return error;
   } else if (
     req.headers.secret_key !== process.env.SECRET_KEY ||
